fa7f122ad522a5402e040c5234de978910914c2e
RishniMeemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2017-12-27 05:52:54 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<!-- BLOG AREA -->
+<section>
+<hr class="no-margin" />
+<div class="blog-container section-content">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="box-layer custom-padding">
+<section>
+<h3> Oh Snap! Page not found!</h3>
+<h3> It seems you're looking for something that isn't here!</h3>
+<a href="<?php echo home_url(); ?>">Click here</a> to go back to homepage! 
+</section>
+</div>
+</div>
+<aside>
+<!-- SIDEBAR AREA -->
+<div class="col-md-3 col-md-offset-1 margin-sidebar">
+<?php get_sidebar(); ?>                                                                                                        	</div>
+</aside>
+</section>
+<?php get_footer(); ?>
